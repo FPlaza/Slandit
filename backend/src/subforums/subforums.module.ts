9c74360a68a23bc6f68c07576cf610b81323464e
Francisco Plaza <francisco.plaza@alumnos.ucn.cl>
2025-11-23 14:58:19 -0300

    correcciones y mongo nube

--- backend/src/subforums/subforums.module.ts
+++ backend/src/subforums/subforums.module.ts
@@ -3,11 +3,13 @@ import { SubforumsService } from './subforums.service';
 import { SubforumsController } from './subforums.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Subforum, SubforumSchema } from './entities/subforums.schema';
+import { Profile, ProfileSchema } from '../profiles/entities/profile.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: Subforum.name, schema: SubforumSchema}
+      { name: Subforum.name, schema: SubforumSchema},
+      { name: Profile.name, schema: ProfileSchema},
     ])
   ],
   controllers: [SubforumsController],
